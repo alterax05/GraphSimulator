7dc03e105cc49608dd882c679ef1c6b528405cca
arloghiro <119437097+arloghiro@users.noreply.github.com>
2024-01-27 09:12:49 +0100

    Correzioni errori messaggio e stampe manca da correggere l'invio dei messaggi

--- WebsocketClientExample/src/Main.java
+++ WebsocketClientExample/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
@@ -6,7 +7,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Inserisci l'ID del nodo: ");
         String nodeId = scanner.next();
@@ -14,18 +15,32 @@ public class Main {
         HttpClient client = HttpClient.newHttpClient();
         WebSocket webSocket = client.newWebSocketBuilder()
                 .buildAsync(URI.create("wss://test-tpsi.barsanti.edu.it?id="+nodeId), new WebSocketListener()).join();
+        System.out.println("Connessione effettuata!");
 
         while (true) {
-            synchronized (System.out) {
+            synchronized (System.out)
+            {
                 System.out.print("Messaggio da inviare ai vicini: ");
             }
 
-            String message = scanner.next();
+            String message = "";
+            do{
+                message = scanner.nextLine();
 
-            synchronized (System.out) {
+            }while(message=="");
+
+            synchronized (System.out)
+            {
                 System.out.print("Inserisci i vicini separati da una virgola: ");
             }
-            String neighborsString = scanner.next();
+
+            String neighborsString = "";
+            do{
+                neighborsString = scanner.nextLine();
+            }while(neighborsString=="");
+
+            System.out.println("Resoconto: " + neighborsString + " " + message);
+
             List<String> neighbors = List.of(neighborsString.split(","));
             StringBuilder jsonMessage = new StringBuilder("{\"message\": \"" + message + "\", \"id\": \"" + nodeId + "\", \"to\": [");;
             for (int i = 0; i < neighbors.size(); i++) {
