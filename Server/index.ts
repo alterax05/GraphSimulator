f967ceab08ab77cdb7b71a8093d118bdf06fb066
Martin Meneghetti <62774491+martinogarrizzo5@users.noreply.github.com>
2024-02-04 12:06:28 +0100

    remove test api

--- Server/index.ts
+++ Server/index.ts
@@ -10,10 +10,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/test", (req, res) => {
-  res.send("HELLo");
-});
-
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
