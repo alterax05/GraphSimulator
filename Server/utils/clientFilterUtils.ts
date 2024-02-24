ec4eb65eabc2fe5e6fb7123ee5ac0f858357ad71
Michele Porcellato <107863063+alterax05@users.noreply.github.com>
2024-02-24 12:57:48 +0100

    Update clientFilterUtils.ts
    Now you can connect up to 32 users

--- Server/utils/clientFilterUtils.ts
+++ Server/utils/clientFilterUtils.ts
@@ -4,7 +4,7 @@ class ClientFilterUtils {
   static isValidId(id: string): boolean {
     // id must start with A, B or C and must be followed by a number between 0 and 25
     const idPattern =
-      /^[ABC]([0-9]|1[0-9]|2[0-5])$|inspector[0-9][0-9][0-9][0-9]$/;
+      /^[ABC]([0-9]|1[0-9]|2[0-9]|3[0-2])$|inspector[0-9][0-9][0-9][0-9]$/;
 
     return idPattern.test(id);
   }
